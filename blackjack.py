bbb9395ee4a06fb7c4ee285e468d9acd8bbfbba4
snhs1106 <stephanieh0611@gmail.com>
2023-10-01 12:59:19 -0400

    Chips class tracks the number of chips a player has based of wins and losses

--- blackjack.py
+++ blackjack.py
@@ -63,3 +63,15 @@ class Hand:
             self.value -= 10
             self.aces -= 1
           
+class Chips:
+    
+    #Keeps track of a player's chips based on win or loss of bets
+    def __init__(self, total = 100):
+        self.total = total  
+        self.bet = 0
+        
+    def win_bet(self):
+        self.total += self.bet
+    
+    def lose_bet(self):
+        self.total -= self.bet
