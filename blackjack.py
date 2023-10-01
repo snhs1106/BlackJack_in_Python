5ec9ecb2c78e793d46bbc3080d6045707ed7ecf8
snhs1106 <stephanieh0611@gmail.com>
2023-10-01 12:56:13 -0400

    Deck class creates a new deck and allows for it to be shuffled and dealed

--- blackjack.py
+++ blackjack.py
@@ -17,3 +17,25 @@ class Card():
     def __str__(self):
         return self.rank + ' of ' + self.suit
 
+class Deck:
+    
+    #Create new deck
+    def __init__(self):
+        self.deck = []
+        for suit in suits:
+            for rank in ranks:
+                self.deck.append(Card(suit,rank))
+    
+    def __str__(self):
+        deck_comp = ''
+        for card in self.deck:
+            deck_comp += '\n' + card.__str__()
+        return 'The deck has: ' + deck_comp
+
+    def shuffle(self):
+        random.shuffle(self.deck)
+
+    #Deal cards to the player and dealer  
+    def deal(self):
+        single_card = self.deck.pop()
+        return single_card
