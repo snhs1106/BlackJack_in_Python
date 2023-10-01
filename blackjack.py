fb7cafac84de47a603fa750b97a42c25dc2a3b9e
snhs1106 <stephanieh0611@gmail.com>
2023-10-01 12:58:09 -0400

    The Hand class allows a player to 'hit' on their hand and adjusts for the value of an ace

--- blackjack.py
+++ blackjack.py
@@ -39,3 +39,27 @@ class Deck:
     def deal(self):
         single_card = self.deck.pop()
         return single_card
+
+class Hand:
+
+    def __init__(self):
+        self.cards = []  # start with an empty list as we did in the Deck class
+        self.value = 0   # start with zero value
+        self.aces = 0    # add an attribute to keep track of aces
+    
+    #Add cards to your hand as you hit or stand
+    def add_card(self,card):
+        self.cards.append(card)
+        self.value += values[card.rank]
+        
+        if card.rank == 'Ace':
+            self.aces += 1
+    
+    def adjust_for_ace(self):
+        
+        #If total value > 21 and I still have an ace
+        #Than change my ace to be 1 instead of 11
+        while self.value > 21 and self.aces:
+            self.value -= 10
+            self.aces -= 1
+          
